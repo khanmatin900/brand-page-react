6f6a5f91df9ee0be0b85f2e58414728e6f5ff024
khanmatin900 <matimkhan480@gmail.com>
2025-01-30 19:16:01 +0530

    fix images

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,12 @@
 import "./App.css";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <nav className="container">
-        <div className="logo">
-          <img src="images/brand_logo.png" alt="logo" />
-        </div>
-        <ul>
-          <li href="#">Menu</li>
-          <li href="#">Location</li>
-          <li href="#">About</li>
-          <li href="#">Contact</li>
-        </ul>
-
-        <button>Login</button>
-      </nav>
-      <main className="hero container">
-        <div className="hero-content ">
-          <h1>YOUR FEET DESERVE THE BEST</h1>
-          <p>
-            YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
-            SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
-            SHOES.
-          </p>
-          <div className="hero-btns">
-            <button>Shop Now</button>
-            <button className="category-btn">Category</button>
-          </div>
-          <div className="shopping">
-            <p>Also Available On</p>
-            <div className="brand-icons">
-              <img src="/images/flipkart.png" alt="flipkart-logo" />
-              <img src="/images/amazon.png" alt="amazon-logo" />
-            </div>
-          </div>
-        </div>
-        <div className="hero-image">
-          <img src="/images/shoe_image.png" alt="shoe" />
-        </div>
-      </main>
+      <Navbar />
+      <Hero />
     </>
   );
 }
